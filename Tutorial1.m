930d6f79d8f0a264e3d534b0666c4d5570a19753
angr12 <rla120@ic.ac.uk>
2022-10-12 13:29:41 +0100

    1st tutorial script

---
+++ Tutorial1.m
@@ -0,0 +1,36 @@
+%% Question 1: dx/dt = -kx
+hold on;
+t_span = [0 10];
+x0 = 4;
+k = 3/5;
+[t, x] = ode45(@(t,x) -k*x, t_span, x0);
+plot(t,x, 'r');
+xlabel("t");
+ylabel("x");
+
+%% Question 2
+x1 = 4*exp(-3/5*t);
+plot(t, x1, 'b');
+hold off
+
+%MSE:
+n = length(x);
+
+error = 0; %initialise error var to add to 
+for i = 1:n
+    diff = x1(i) - x(i);
+    diff = diff.^2;
+    error = error + diff;
+end
+
+mse = error/n;
+disp(mse);
+
+%% Question 3
+figure(2)
+a = zeros(1, n-1);
+for i = 1:n-1
+    a(i) = t(i+1)-t(i);
+end
+
+plot(a);
